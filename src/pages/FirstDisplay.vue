1f06f865f4b4e008508f344e521a6a5e92c7e503
tmpbook <nauy2011@126.com>
2017-03-21 23:46:24 +0800

    asd

--- src/pages/FirstDisplay.vue
+++ src/pages/FirstDisplay.vue
@@ -49,19 +49,19 @@
                 <el-tag type="success">{{ taskList[0].State }}</el-tag>
               </el-form-item>
               <el-form-item label="任务ID:">
-                <el-tag type="success">{{ taskList[0].TaskId }}</el-tag>
+                <el-tag type="primary">{{ taskList[0].TaskId }}</el-tag>
               </el-form-item>
               <el-form-item label="开始时间:">
-                <el-tag type="success">{{ taskList[0].StartTime | time_formatter }}</el-tag>
+                <el-tag type="primary">{{ taskList[0].StartTime | time_formatter }}</el-tag>
               </el-form-item>
               <el-form-item label="结束时间:">
-                <el-tag type="success">{{ taskList[0].EndTime | time_formatter }}</el-tag>
+                <el-tag type="primary">{{ taskList[0].EndTime | time_formatter }}</el-tag>
               </el-form-item>
               <el-form-item label="CPUUsage:">
-                <el-tag type="success">{{ taskList[0].CPUUsage }}</el-tag>
+                <el-tag type="primary">{{ taskList[0].CPUUsage }}</el-tag>
               </el-form-item>
               <el-form-item label="9 分钱可以处理相同图片:">
-                <el-tag type="success">{{ count }}</el-tag> 张
+                <el-tag type="danger">{{ count }}</el-tag> 张
               </el-form-item>
             </el-form>
             <i v-else class="el-icon-time"> 请先提交任务</i>
